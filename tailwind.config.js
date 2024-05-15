337877e48c83f2e49a12d414fcf6ca13280243c4
zahra-gm <zahra.gerami78@gmail.com>
2024-05-15 22:23:43 +0430

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+      extend: {
+        fontFamily:{
+          'sans':['Vazirmatn'],
+        },
+        colors: {
+          'secondary': '#3F4264',
+          'grayT':'#36385380',
+          'primary':'#4A6DFF',
+          'redprimary':'#FF0000CC',
+          'bluesecondary':'#555775',
+          'orange':'#FF9900',
+        },
+      },
+    },
+    plugins: [],
+  }
